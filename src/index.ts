00cdf73e1f8fc9deae66ffc6403380be73869a51
styx11 <1654630248@qq.com>
2019-04-07 20:22:20 +0800

    :sparkles: add ON emitter func

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-import { Events } from './interface';
+import {
+  Events,
+  Callback,
+} from './interface';
 
 export default class Emitter {
   private _events: Events;
   constructor () {
     this._events = Object.create(null);
+  };
+  on (event: string | Array<string>, cb: Callback): this {
+    if (Array.isArray(event)) {
+      const length: number = event.length;
+      for (let i=0; i<length; i++) {
+        this.on(event[i], cb);
+      }
+      return this;
+    }
+    if (!Array.isArray(this._events[event])) {
+      const cbs: Array<Function> = [];
+      this._events[event] = cbs;
+    }
+    this._events[event].push(cb);
+    return this;
   };
 }
