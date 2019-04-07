224bff6f62a6a9332e54b4b8d57d7a0be39e3a7a
styx11 <1654630248@qq.com>
2019-04-07 20:12:24 +0800

    :sparkles: update interface of callback function

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,4 +1,9 @@
 // Events 为一个索引类型，用于描述索引类型（string）和返回值（Array<Function>）
 export interface Events {
-  [event: string]: Array<Function>
+  [event: string]: Array<Function>;
+}
+
+// 绑定至 event 的回调函数
+export interface Callback {
+  (...args: Array<any>): any;
 }
