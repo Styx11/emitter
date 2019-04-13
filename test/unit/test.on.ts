614fed7bd812c763b274d4af233f23818a8a1568
styx11 <1654630248@qq.com>
2019-04-13 11:23:13 +0800

    :white_check_mark: update unit test of ON

--- test/unit/test.on.ts
+++ test/unit/test.on.ts
@@ -1,41 +1,63 @@
-import Emitter from '../src/index';
-import { Callback } from '../src/interface';
-const assert = require('assert');
+import assert from 'assert';
+import Emitter from '../../src/index';
+import { Callback } from '../../src/interface';
 
-let result: boolean = true;
-const emitter = new Emitter();
-const event1: string = 'event1';
-const event2: string = 'event2';
-const eventArr: Array<string> = ['event3', 'event4', 'event5'];
-const cb: Callback = (...arg: Array<any>): any => {};
+export default function unitTestOn (): boolean {
+  let res: boolean = true;
+  const emitter = new Emitter();
+  const event1: string = 'event1';
+  const event2: string = 'event2';
+  const eventArr: Array<string> = ['event3', 'event4', 'event5'];
+  const cb: Callback = (...arg: Array<any>): any => {};
 
-emitter.on(event1, cb);
-emitter.on(event2, cb);
-emitter.on(eventArr, cb);
-try {
-  assert.strictEqual(emitter.listenerCount(event1), 1);
-  assert.strictEqual(emitter.listenerCount(event2), 1);
-  assert.strictEqual(emitter.listenerCount('event3'), 1);
-  assert.strictEqual(emitter.listenerCount('event4'), 1);
-  assert.strictEqual(emitter.listenerCount('event5'), 1);
-  console.log('test:ON group.1 all past');
-} catch (e) {
-  result = false;
-  console.error(e);
+  try {
+    emitter.on(event1, cb);
+    emitter.on(event2, cb);
+    emitter.on(eventArr, cb);
+    assert.strictEqual(emitter.listenerCount(event1), 1);
+    assert.strictEqual(emitter.listenerCount(event2), 1);
+    assert.strictEqual(emitter.listenerCount('event3'), 1);
+    assert.strictEqual(emitter.listenerCount('event4'), 1);
+    assert.strictEqual(emitter.listenerCount('event5'), 1);
+    console.log('test:ON group.1 all past');
+  } catch (e) {
+    res = false;
+    console.log('test:ON group.1 has failed');
+    console.error(e);
+  }
+
+  try {
+    emitter.on(event1, cb);
+    emitter.on(event2, cb);
+    emitter.on(eventArr, cb);
+    assert.strictEqual(emitter.listenerCount(event1), 2);
+    assert.strictEqual(emitter.listenerCount(event2), 2);
+    assert.strictEqual(emitter.listenerCount('event3'), 2);
+    assert.strictEqual(emitter.listenerCount('event4'), 2);
+    assert.strictEqual(emitter.listenerCount('event5'), 2);
+    console.log('test:ON group.2 all past');
+  } catch (err) {
+    res = false;
+    console.log('test:ON group.2 has failed');
+    console.error(err);
+  }
+
+  try {
+    emitter.on(event1, cb);
+    emitter.on(event2, cb);
+    emitter.on('event3', cb);
+    emitter.on('event4', cb);
+    emitter.on('event5', cb);
+    assert.strictEqual(emitter.listenerCount(event1), 3);
+    assert.strictEqual(emitter.listenerCount(event2), 3);
+    assert.strictEqual(emitter.listenerCount('event3'), 3);
+    assert.strictEqual(emitter.listenerCount('event4'), 3);
+    assert.strictEqual(emitter.listenerCount('event5'), 3);
+    console.log('test:ON group.3 all past');
+  } catch (err) {
+    res = false;
+    console.log('test:ON group.3 has failed');
+    console.error(err);
+  }
+  return res;
 }
-
-emitter.on(event1, cb);
-emitter.on(event2, cb);
-emitter.on(eventArr, cb);
-try {
-  assert.strictEqual(emitter.listenerCount(event1), 2);
-  assert.strictEqual(emitter.listenerCount(event2), 2);
-  assert.strictEqual(emitter.listenerCount('event3'), 2);
-  assert.strictEqual(emitter.listenerCount('event4'), 2);
-  assert.strictEqual(emitter.listenerCount('event5'), 2);
-  console.log('test:ON group.2 all past');
-} catch (err) {
-  result = false;
-  console.error(err);
-}
-export default result;
