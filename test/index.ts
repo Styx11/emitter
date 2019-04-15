61caf23b4859b75a70d1b577887bd2313e8986b8
styx11 <1654630248@qq.com>
2019-04-15 20:24:01 +0800

    :white_check_mark: update index test

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,10 @@
 import assert from 'assert';
 import unitTestOn from './unit/test.on';
+import unitTestOff from './unit/test.off';
 
 try {
   assert.ok(unitTestOn());
+  assert.ok(unitTestOff());
 
   console.log('TESTS ALL PAST');
 } catch (e) {
